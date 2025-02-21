8c42f1a5a9d789ff85af039d391d8390ad65ff23
gruposmi <gabriel.souza@gruposmi.com.br>
2025-02-21 14:10:27 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -65,7 +65,7 @@ function removerAcentos(texto) {
 
 document.addEventListener("DOMContentLoaded", function () {
     // Carrega os ramais a partir do arquivo JSON
-    fetch("https://gruposmi.github.io/ramais.gruposmi/data.json")
+    fetch("https://gabriel-pagani.github.io/database/data.json")
       .then(response => response.json())
       .then(data => {
         // Substitui a listaRamais hard-coded pelos dados carregados do JSON
